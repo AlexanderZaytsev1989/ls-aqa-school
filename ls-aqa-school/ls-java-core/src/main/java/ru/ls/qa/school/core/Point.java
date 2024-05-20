d06d0abd589d02775b4edea36fcc14e18e1a2d15
Александр Зайцев <zeus89@yandex.ru>
2024-05-20 10:10:31 +0300

    Добавил класс Point и метод public double distance. Задания 2.1 и 2.2

---
+++ ls-aqa-school/ls-java-core/src/main/java/ru/ls/qa/school/core/Point.java
@@ -0,0 +1,17 @@
+package ru.ls.qa.school.core;
+
+public class Point {
+    double x;
+    double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point point){
+     double x = 5.0;
+     double y = 6.0;
+     return Math.sqrt(Math.pow(this.y - this.x, 2) + Math.pow(y - x, 2));
+    }
+}
