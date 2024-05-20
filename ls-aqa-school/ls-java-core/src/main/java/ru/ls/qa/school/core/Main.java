13d1217690c53a496a151fde8e20d1054b9ca955
Александр Зайцев <zeus89@yandex.ru>
2024-05-20 09:14:12 +0300

    Добавил класс Point и метод public double distance. Задания 2.1 и 2.2

--- ls-aqa-school/ls-java-core/src/main/java/ru/ls/qa/school/core/Main.java
+++ ls-aqa-school/ls-java-core/src/main/java/ru/ls/qa/school/core/Main.java
@@ -3,6 +3,8 @@ package ru.ls.qa.school.core;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        Point p = new Point(5.0, 5.0);
+
+        System.out.println(p.distance(p));
     }
 }
