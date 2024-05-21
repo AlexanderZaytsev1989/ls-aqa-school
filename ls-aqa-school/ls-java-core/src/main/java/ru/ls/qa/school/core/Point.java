8729685841d2c6b9d62888cce341de5673369468
13th <pavel.voronin@ligastavok.ru>
2024-05-21 15:31:26 +0300

    Отформатировал код

--- ls-aqa-school/ls-java-core/src/main/java/ru/ls/qa/school/core/Point.java
+++ ls-aqa-school/ls-java-core/src/main/java/ru/ls/qa/school/core/Point.java
@@ -4,14 +4,14 @@ public class Point {
     double x;
     double y;
 
-    public Point(double x, double y){
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
     }
 
-    public double distance(Point point){
-     double x = 5.0;
-     double y = 6.0;
-     return Math.sqrt(Math.pow(this.y - this.x, 2) + Math.pow(y - x, 2));
+    public double distance(Point point) {
+        double x = 5.0;
+        double y = 6.0;
+        return Math.sqrt(Math.pow(this.y - this.x, 2) + Math.pow(y - x, 2));
     }
 }
