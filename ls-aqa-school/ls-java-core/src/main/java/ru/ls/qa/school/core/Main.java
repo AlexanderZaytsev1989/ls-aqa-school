078a4239be949dfe8fb826b115ed77b65c2d2ad2
Александр Зайцев <zeus89@yandex.ru>
2024-05-21 08:28:36 +0300

    Добавил запускаемый класс PointTask

--- ls-aqa-school/ls-java-core/src/main/java/ru/ls/qa/school/core/Main.java
+++ ls-aqa-school/ls-java-core/src/main/java/ru/ls/qa/school/core/Main.java
@@ -2,7 +2,5 @@ package ru.ls.qa.school.core;
 
 public class Main {
     public static void main(String[] args) {
-        Point p =  new Point(5.0, 5.0);
-        System.out.println(p.distance(p));
     }
 }
---
+++ ls-aqa-school/ls-java-core/src/main/java/ru/ls/qa/school/core/PointTask.java
@@ -0,0 +1,8 @@
+package ru.ls.qa.school.core;
+
+public class PointTask {
+    public static void main(String[] args) {
+        Point p =  new Point(5.0, 5.0);
+        System.out.println(p.distance(p));
+    }
+}
